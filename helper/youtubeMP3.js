13826e64ecd3a7e005c731dc93c01b68687c668f
= <=>
2024-06-27 22:12:58 +0700

    Update Thumnail

--- helper/youtubeMP3.js
+++ helper/youtubeMP3.js
@@ -1,8 +1,10 @@
 const poji_ytmp3 = require('youtube-to-mp3-poji');
+var youtubeThumbnail = require('youtube-thumbnail');
 const downloadMP3 = async function(url){
     try {
         const data = await poji_ytmp3(url); // link youtube
-        return data
+        let thumbnail = youtubeThumbnail(url);
+        return {data,thumbnail,info}
       } catch (error) {
         return error.message;
       }
